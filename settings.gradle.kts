6e0526700daf5caa23fef5e0b4abdd9d8b3be665
fabtelo <antelo.fabian@gmail.com>
2023-10-22 08:09:13 -0400

    primer commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ADM_PERSONAL"
+include(":app")
