870753e704583c5d0a03279c58ab2e6a8888bbfe
fabtelo <antelo.fabian@gmail.com>
2023-10-23 17:22:37 -0400

    ya funciona el google auth y tiene parte del xml

--- app/src/main/java/com/example/adm_personal/SecondActivity.java
+++ app/src/main/java/com/example/adm_personal/SecondActivity.java
@@ -3,17 +3,19 @@ package com.example.adm_personal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SecondActivity extends AppCompatActivity {
     private DatabaseReference nodo= FirebaseDatabase.getInstance().getReference("P-ASTX1");
-
+    private Button registroB,turnosB,permisosB,entrandoB,saliendoB,enpozoB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
 
     }
 }
